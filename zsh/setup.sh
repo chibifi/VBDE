08a548b1ab8adfb967f067e14e067e1c9ebcbe99
Catalyn45 <ancutei.catalin@gmail.com>
2022-08-19 06:31:30 -0400

    Final fix

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -12,14 +12,16 @@ unzip FiraCode.zip
 rm FiraCode.zip
 cd -
 
-rm ~/.zshrc
-ln -s $PWD/.zshrc ~/
-
 export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-zsh -c "p10k configure"
-zsh -c "git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k"
+rm ~/.zshrc
+ln -s $PWD/.zshrc ~/
+
+source ~/.zshrc
+
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+p10k configure
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
