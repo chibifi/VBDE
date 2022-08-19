b2852d0849fbfc60fdbe80357ba4ddca0d8e4fec
Catalyn45 <ancutei.catalin@gmail.com>
2022-08-19 13:19:48 +0300

    Fixed installer

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -19,8 +19,8 @@ export RUNZSH=no
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 zsh -c "p10k configure"
+zsh -c "git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k"
 
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
