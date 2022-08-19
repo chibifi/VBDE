a06fb2ff09d905ae1354aa373c21fecf807cac64
Catalyn45 <ancutei.catalin@gmail.com>
2022-08-19 13:16:06 +0300

    Fixed installer

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -20,11 +20,9 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 
 source ~/.zshrc
 
-p10k configure
+zsh -c "p10k configure"
 
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
-source ~/.zshrc
 
